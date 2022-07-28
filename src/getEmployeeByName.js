44c68e6f357a7df4ed9c1c01e03520b2cd5d7d99
Elivelton <eliveltonmn@outlook.com>
2022-07-28 16:52:55 -0300

    Terceiro requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  return employees.reduce((acc, curr) =>
+    (curr.firstName === employeeName || curr.lastName === employeeName ? curr : acc), {});
 }
+
+console.log(getEmployeeByName('Elser'));
+console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
