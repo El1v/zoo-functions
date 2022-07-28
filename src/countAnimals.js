e45ac0c78ad0d2effe1f6e080973d98d445a985b
Elivelton <eliveltonmn@outlook.com>
2022-07-28 19:40:45 -0300

    Quinto requisito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  let retorno = { };
+  if (typeof animal === 'undefined') {
+    retorno = species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+    return retorno;
+  }
+  if (Object.keys(animal).length === 1) {
+    return species.find((element) => element.name === animal.specie).residents.length;
+  }
+  return species.find((element) => element.name === animal.specie)
+    .residents.filter((element) => element.sex === animal.sex).length;
 }
+
+// console.log(countAnimals());
+// console.log(countAnimals({ specie: 'penguins' }));
+console.log(countAnimals({ specie: 'bears', sex: 'female' }));
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 
 module.exports = countAnimals;
