5b31e346ae724d0028c10fea8b19d80fbcec0a2e
Elivelton <eliveltonmn@outlook.com>
2022-07-29 13:05:33 -0300

    Nono requisito

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,47 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const { species, hours } = data;
+
+function weekDays(day) {
+  let exhibition = '';
+  let officeHour = '';
+  if (day === 'Monday') {
+    exhibition = 'The zoo will be closed!';
+    officeHour = 'CLOSED';
+  } else {
+    exhibition = species.filter((element) => element.availability
+      .includes(day)).map((element) => element.name);
+    officeHour = `Open from ${hours[day].open}am until ${hours[day].close}pm`;
+  }
+  const result = {
+    officeHour,
+    exhibition,
+  };
+  return result;
 }
+
+function getSchedule(scheduleTarget) {
+  const scheduleDay = {};
+  const days = Object.keys(hours);
+  const animals = species.map((element) => element.name);
+  if (days.includes(scheduleTarget)) {
+    scheduleDay[scheduleTarget] = weekDays(scheduleTarget);
+    return scheduleDay;
+  }
+  if (scheduleTarget === undefined || !animals.includes(scheduleTarget)) {
+    days.reduce((acc, curr) => {
+      // console.log(scheduleDay);
+      scheduleDay[curr] = weekDays(curr);
+    }, {});
+    return scheduleDay;
+  }
+  return species.find((element) => element.name === scheduleTarget).availability;
+}
+
+// console.log(getSchedule('Tuesday'));
+// console.log(getSchedule('Monday'));
+// console.log(getSchedule('penguins'));
+// console.log(getSchedule('batata'));
+// console.log(getSchedule());
 
 module.exports = getSchedule;
