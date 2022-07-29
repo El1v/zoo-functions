261c2f363756b5d1469aea17fd740156ad528262
Elivelton <eliveltonmn@outlook.com>
2022-07-29 18:00:40 -0300

    Requisito 11 - aleluia

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,38 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species, employees } = data;
+
+function getSpecies(idAnimal) {
+  const animal = species.find((element) => element.id === idAnimal);
+  return {
+    species: animal.name,
+    locations: animal.location,
+  };
+}
+
+const getEmplooyeeById = (id) => employees.find((element) => element.id === id);
+const getEmplooyeeByName = (name) => employees.find((element) =>
+  element.firstName === name || element.lastName === name);
+
+const getEmployee = (employee) => {
+  const speciesList = employee.responsibleFor.map((getSpecies));
+  return {
+    id: employee.id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: speciesList.map((element) => element.species),
+    locations: speciesList.map((element) => element.locations),
+  };
+};
+
+function getEmployeesCoverage(obj) {
+  if (obj === undefined) {
+    return employees.map(getEmployee);
+  }
+  if (getEmplooyeeById(obj.id) === undefined && getEmplooyeeByName(obj.name) === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  const employee = obj.name ? getEmplooyeeByName(obj.name) : getEmplooyeeById(obj.id);
+  return getEmployee(employee);
 }
 
 module.exports = getEmployeesCoverage;
